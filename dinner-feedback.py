b2b45fafbec62028bc4e1f0cbc441a34fb22e8fa
Eric Cady <ecady@Eric-MBPr.hsd1.ma.comcast.net>
2016-03-08 21:03:48 -0500

    starting out

--- dinner-feedback.py
+++ dinner-feedback.py
@@ -4,4 +4,4 @@ print ( "hey there, what did you have for dinner?" )
 
 dinner_answer = raw_input()
 
-print ( "oh, you had " + dinner_answer + ", was it good?" )
+print ( "oh really, you had " + dinner_answer + ", was it good?" )
