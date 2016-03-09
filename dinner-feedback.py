bcc04944ea5262c98a10ff97eaa8741a87b7e4d0
Eric Cady <ecady@Eric-MBPr.hsd1.ma.comcast.net>
2016-03-09 08:17:45 -0500

    latest

--- dinner-feedback.py
+++ dinner-feedback.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+#import time to use and put a time stamp at the start
+import datetime
+print datetime.datetime.now()
+
 #just trying to make a simple script - this will ask the question about dinner
 print ( "hey there, what did you have for dinner?" )
 
@@ -18,4 +22,18 @@ if like_answer in ['y', 'Y', 'yes', 'Yes', 'YES']:
 else:
 	print ( "sorry you didn't like it" )
 
+#now that we have that out of the way - I want to get a list of s3 buckets
+#so I need to import the awscli
+#import awscli
+#aws s3 ls
 
+#import boto.ec2
+
+#did that work?
+print ( "did that work?" )
+
+#sleep - I can only do this because I imported the date time thing - but this is sleep for 15 seconds
+time.sleep(15)
+
+#also use time stamp at the end
+print datetime.datetime.now()
